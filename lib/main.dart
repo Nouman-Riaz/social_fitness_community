5d58f81efe5f5b32c3e24ce751b07a5fe0648c70
Nouman-Riaz <noumanriaz1177@gmail.com>
2024-02-24 21:42:52 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:softech/modules/enthusiast_view/bloc/enthusiast_bloc.dart';
-import 'package:softech/modules/fitness_professional/bloc/professional_bloc.dart';
+
+import 'package:provider/provider.dart';
+
 import 'package:softech/modules/login/bloc/login_bloc.dart';
 import 'package:softech/modules/personal_information/view/personal_name_view.dart';
 import 'package:softech/modules/signup/bloc/signup_bloc.dart';
 import 'package:softech/modules/splash/splash_view.dart';
 
+import 'modules/auth_manager.dart';
+import 'modules/enthusiast_view/bloc/enthusiast_bloc.dart';
+import 'modules/fitness_professional/bloc/professional_bloc.dart';
 import 'modules/personal_information/bloc/personal_info_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // SharedPreferences prefs = await SharedPreferences.getInstance();
+  // bool isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+  // runApp(MultiProvider(
+  //   providers: [ChangeNotifierProvider(
+  //       create: (context) => SettingsManager(isDarkTheme: isDarkTheme)),
+  //   ],
+  //   child: MyApp(),
+  // ),);
+  // runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthManager(),
+      child: MyApp(),
+    ),
+  );
 }
+
 
 late double width;
 late double height;
